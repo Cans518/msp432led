f83ff551ecd71524f9e82dea64ae0485c24c3b07
Mzee <798061533@qq.com>
2023-09-20 00:20:24 +0800

    优化注释

--- lib/LED_S/LED_S.c
+++ lib/LED_S/LED_S.c
@@ -1,11 +1,25 @@
 #include<LED_S.h>
 
+void LED_Init(void){LED1_Init();LED2_Init();}
 void LED1_Init(void){P1DIR |= BIT0;}
 void LED2_Init(void){P2DIR |= BIT0 | BIT1 | BIT2;LED2_BLUE=0;LED2_RED=0;LED2_GREEN=0;}
+
 
 void LED1_RED_ON(void){LED1_RED = 1;}
 void LED1_RED_OFF(void){LED1_RED = 0;}
 void LED1_RED_TOGGLE(void){LED1_RED ^= 1;}
+
+void LED2_RED_ON(void){LED2_RED = 1;}
+void LED2_RED_OFF(void){LED2_RED = 0;}
+void LED2_RED_TOGGLE(void){LED2_RED ^= 1;}
+
+void LED2_GREEN_ON(void){LED2_GREEN = 1;}
+void LED2_GREEN_OFF(void){LED2_GREEN = 0;}
+void LED2_GREEN_TOGGLE(void){LED2_GREEN ^= 1;}
+
+void LED2_BLUE_ON(void){LED2_BLUE = 1;}
+void LED2_BLUE_OFF(void){LED2_BLUE = 0;}
+void LED2_BLUE_TOGGLE(void){LED2_BLUE ^= 1;}
 
 void LED2_YELLOW_ON(void){LED2_RED = 1;LED2_GREEN = 1;}
 void LED2_YELLOW_OFF(void){LED2_RED = 0;LED2_GREEN = 0;}
--- src/main.c
+++ src/main.c
@@ -12,13 +12,19 @@ int main()
     //初始化P2.0,并点亮
 	LED2_Init();
 	LED2_YELLOW_ON();
+	LED2_BLUE_OFF();
 
 	//当key1或者key2按下时，开关LED
 	while(1){
-		for (uint16_t i  = 0; i < 3000; i++){}
-		if(KEY1 == KEY_ON || KEY2 == KEY_ON){
-			for (uint16_t i  = 0; i < 3000; i++){}
+		if(KEY1 == KEY_ON){
+			for (uint16_t i  = 0;i <= 500; i++){}
+			for (uint16_t i  = 0;; i++){if(KEY1 == KEY_OFF)break;}
 			LED2_YELLOW_TOGGLE();
+		}
+		if(KEY2 == KEY_ON){
+			for (uint16_t i  = 0;i <= 500; i++){}
+			for (uint16_t i  = 0;; i++){if(KEY2 == KEY_OFF)break;}
+			LED2_BLUE_TOGGLE();
 		}
 	}
 
