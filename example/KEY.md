b4b1df481b41b53f5e1175b177f35372c7a85d96
Mzee <798061533@qq.com>
2023-09-22 22:02:26 +0800

    update for week2

--- example/KEY.md
+++ example/KEY.md
@@ -30,3 +30,39 @@ int main()
     return 0;
 }
 ```
+
+```c
+#include <driverlib.h>
+#include <KEY_S.h>
+#include <LED_S.h>
+
+int main()
+{
+    //关闭看门狗
+    MAP_WDT_A_holdTimer();
+
+	KEY_Init();
+
+    //初始化P2.0,并点亮
+	LED2_Init();
+	LED2_YELLOW_ON();
+	LED2_BLUE_OFF();
+
+	//当key1或者key2按下时，开关LED
+	while(1){
+		if(KEY1 == KEY_ON){
+			for (uint16_t i  = 0;i <= 500; i++){}
+			for (uint16_t i  = 0;; i++){if(KEY1 == KEY_OFF)break;}
+			LED2_YELLOW_TOGGLE();
+		}
+		if(KEY2 == KEY_ON){
+			for (uint16_t i  = 0;i <= 500; i++){}
+			for (uint16_t i  = 0;; i++){if(KEY2 == KEY_OFF)break;}
+			LED2_BLUE_TOGGLE();
+		}
+	}
+
+
+    return 0;
+}
+```
--- src/main.c
+++ src/main.c
@@ -1,33 +1,42 @@
 #include <driverlib.h>
-#include <KEY_S.h>
-#include <LED_S.h>
+
+volatile uint32_t Timer32_Cnt = 0;
+/* Timer32 ISR */
 
 int main()
 {
     //关闭看门狗
     MAP_WDT_A_holdTimer();
 
-	KEY_Init();
-
     //初始化P2.0,并点亮
-	LED2_Init();
-	LED2_YELLOW_ON();
-	LED2_BLUE_OFF();
+    P2DIR |= BIT0 | BIT1;
+    P2OUT |= BIT0 | BIT1;
+    
+	//配置timer32
+	MAP_Timer32_initModule(TIMER32_BASE, TIMER32_PRESCALER_1, TIMER32_32BIT,TIMER32_PERIODIC_MODE);
+	//开启中断
+	MAP_Interrupt_enableInterrupt(INT_T32_INT1);
+	//开启总中断
+	MAP_Interrupt_enableMaster();
+	//开启定时器
+	MAP_Timer32_setCount(TIMER32_BASE,750000);
+    MAP_Timer32_enableInterrupt(TIMER32_BASE);
+    MAP_Timer32_startTimer(TIMER32_BASE, true);
 
-	//当key1或者key2按下时，开关LED
-	while(1){
-		if(KEY1 == KEY_ON){
-			for (uint16_t i  = 0;i <= 500; i++){}
-			for (uint16_t i  = 0;; i++){if(KEY1 == KEY_OFF)break;}
-			LED2_YELLOW_TOGGLE();
-		}
-		if(KEY2 == KEY_ON){
-			for (uint16_t i  = 0;i <= 500; i++){}
-			for (uint16_t i  = 0;; i++){if(KEY2 == KEY_OFF)break;}
-			LED2_BLUE_TOGGLE();
-		}
-	}
-
-
+    
     return 0;
+}
+
+
+void T32_INT1_IRQHandler(void)
+{
+	MAP_Timer32_clearInterruptFlag(TIMER32_BASE);
+	Timer32_Cnt +=10;
+	if(Timer32_Cnt == 100)
+	{
+		Timer32_Cnt = 0;
+		P2OUT ^= BIT0 | BIT1;
+	}
+    MAP_Timer32_setCount(TIMER32_BASE,750000);
+	MAP_Timer32_startTimer(TIMER32_BASE, true);
 }
