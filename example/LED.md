89a1df88d50f8af2e17ef9f149c0956cc7352ab3
Mzee <798061533@qq.com>
2023-09-26 09:32:26 +0800

    呼吸灯

--- example/LED.md
+++ example/LED.md
@@ -135,3 +135,75 @@ void T32_INT1_IRQHandler(void)
 	MAP_Timer32_startTimer(TIMER32_BASE, true);
 }
 ```
+
+### 呼吸灯测试
+
+```c
+#include <driverlib.h>
+
+#define TIMER_PERIODA0 15000
+#define DUTY_CYCLE0 300
+
+int compare_light = 300;
+int flag = 1;
+
+//结构体A0设定
+const Timer_A_UpModeConfig upConfigA0 =
+{
+        TIMER_A_CLOCKSOURCE_SMCLK,              // SMCLK Clock SOurce
+        TIMER_A_CLOCKSOURCE_DIVIDER_1,          // SMCLK/1 = 3MHz
+        TIMER_PERIODA0,                           // 6000 tick period
+        TIMER_A_TAIE_INTERRUPT_DISABLE,         // Disable Timer interrupt
+        TIMER_A_CCIE_CCR0_INTERRUPT_DISABLE,    // Disable CCR0 interrupt
+        TIMER_A_DO_CLEAR                        // Clear value
+};
+
+//配置PWM波
+Timer_A_CompareModeConfig compareConfig_PWM0 =
+{
+        TIMER_A_CAPTURECOMPARE_REGISTER_1,          // Use CCR1
+        TIMER_A_CAPTURECOMPARE_INTERRUPT_DISABLE,   // Disable CCR interrupt
+        TIMER_A_OUTPUTMODE_TOGGLE_SET,              // Toggle output but
+        DUTY_CYCLE0                                // 300 Duty Cycle
+};
+
+const uint8_t port_mapping[]=
+{
+	PMAP_TA0CCR1A,PMAP_TA0CCR1A,
+	PMAP_NONE,PMAP_NONE,PMAP_NONE,PMAP_NONE,PMAP_NONE
+};
+
+
+int main()
+{
+    //关闭看门狗
+    MAP_WDT_A_holdTimer();
+
+	//映射引脚
+	MAP_PMAP_configurePorts((const uint8_t*)port_mapping,PMAP_P2MAP,1,PMAP_DISABLE_RECONFIGURATION);
+	MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2,GPIO_PIN0|GPIO_PIN1,GPIO_PRIMARY_MODULE_FUNCTION);
+
+	//配置定时器A0
+	MAP_Timer_A_configureUpMode(TIMER_A0_BASE, &upConfigA0);
+	MAP_Timer_A_startCounter(TIMER_A0_BASE, TIMER_A_UP_MODE);
+	
+	//开启PWM波
+	MAP_Timer_A_initCompare(TIMER_A0_BASE, &compareConfig_PWM0);
+
+	while (1)
+	{
+		compare_light += flag * 100;
+		if(compare_light >= TIMER_PERIODA0)
+			flag = -1;
+		if (compare_light <= 0)
+			flag = 1;
+		compareConfig_PWM0.compareValue = compare_light;
+		MAP_Timer_A_initCompare(TIMER_A0_BASE, &compareConfig_PWM0);
+		for(int i=0;i<10000;i++){};
+	}
+	
+
+    
+    return 0;
+}
+```
--- src/main.c
+++ src/main.c
@@ -1,42 +1,67 @@
 #include <driverlib.h>
 
-volatile uint32_t Timer32_Cnt = 0;
-/* Timer32 ISR */
+#define TIMER_PERIODA0 15000
+#define DUTY_CYCLE0 300
+
+int compare_light = 300;
+int flag = 1;
+
+//结构体A0设定
+const Timer_A_UpModeConfig upConfigA0 =
+{
+        TIMER_A_CLOCKSOURCE_SMCLK,              // SMCLK Clock SOurce
+        TIMER_A_CLOCKSOURCE_DIVIDER_1,          // SMCLK/1 = 3MHz
+        TIMER_PERIODA0,                           // 6000 tick period
+        TIMER_A_TAIE_INTERRUPT_DISABLE,         // Disable Timer interrupt
+        TIMER_A_CCIE_CCR0_INTERRUPT_DISABLE,    // Disable CCR0 interrupt
+        TIMER_A_DO_CLEAR                        // Clear value
+};
+
+//配置PWM波
+Timer_A_CompareModeConfig compareConfig_PWM0 =
+{
+        TIMER_A_CAPTURECOMPARE_REGISTER_1,          // Use CCR1
+        TIMER_A_CAPTURECOMPARE_INTERRUPT_DISABLE,   // Disable CCR interrupt
+        TIMER_A_OUTPUTMODE_TOGGLE_SET,              // Toggle output but
+        DUTY_CYCLE0                                // 300 Duty Cycle
+};
+
+const uint8_t port_mapping[]=
+{
+	PMAP_TA0CCR1A,PMAP_TA0CCR1A,
+	PMAP_NONE,PMAP_NONE,PMAP_NONE,PMAP_NONE,PMAP_NONE
+};
+
 
 int main()
 {
     //关闭看门狗
     MAP_WDT_A_holdTimer();
 
-    //初始化P2.0,并点亮
-    P2DIR |= BIT0 | BIT1;
-    P2OUT |= BIT0 | BIT1;
-    
-	//配置timer32
-	MAP_Timer32_initModule(TIMER32_BASE, TIMER32_PRESCALER_1, TIMER32_32BIT,TIMER32_PERIODIC_MODE);
-	//开启中断
-	MAP_Interrupt_enableInterrupt(INT_T32_INT1);
-	//开启总中断
-	MAP_Interrupt_enableMaster();
-	//开启定时器
-	MAP_Timer32_setCount(TIMER32_BASE,750000);
-    MAP_Timer32_enableInterrupt(TIMER32_BASE);
-    MAP_Timer32_startTimer(TIMER32_BASE, true);
+	//映射引脚
+	MAP_PMAP_configurePorts((const uint8_t*)port_mapping,PMAP_P2MAP,1,PMAP_DISABLE_RECONFIGURATION);
+	MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2,GPIO_PIN0|GPIO_PIN1,GPIO_PRIMARY_MODULE_FUNCTION);
+
+	//配置定时器A0
+	MAP_Timer_A_configureUpMode(TIMER_A0_BASE, &upConfigA0);
+	MAP_Timer_A_startCounter(TIMER_A0_BASE, TIMER_A_UP_MODE);
+	
+	//开启PWM波
+	MAP_Timer_A_initCompare(TIMER_A0_BASE, &compareConfig_PWM0);
+
+	while (1)
+	{
+		compare_light += flag * 100;
+		if(compare_light >= TIMER_PERIODA0)
+			flag = -1;
+		if (compare_light <= 0)
+			flag = 1;
+		compareConfig_PWM0.compareValue = compare_light;
+		MAP_Timer_A_initCompare(TIMER_A0_BASE, &compareConfig_PWM0);
+		for(int i=0;i<10000;i++){};
+	}
+	
 
     
     return 0;
-}
-
-
-void T32_INT1_IRQHandler(void)
-{
-	MAP_Timer32_clearInterruptFlag(TIMER32_BASE);
-	Timer32_Cnt +=10;
-	if(Timer32_Cnt == 100)
-	{
-		Timer32_Cnt = 0;
-		P2OUT ^= BIT0 | BIT1;
-	}
-    MAP_Timer32_setCount(TIMER32_BASE,750000);
-	MAP_Timer32_startTimer(TIMER32_BASE, true);
 }
